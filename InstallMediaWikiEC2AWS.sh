98fc2d89a6b4d7d611b03cdc7319a7dffb3ab7fe
ImmLearning <51955196+ImmLearning@users.noreply.github.com>
2019-10-22 12:26:36 +0530

    c

--- InstallMediaWikiEC2AWS.sh
+++ InstallMediaWikiEC2AWS.sh
@@ -1,5 +1,6 @@
 #readme - after procuring infra side using cloud formation execute below command
-#
+#wget -O - https://raw.githubusercontent.com/ImmLearning/MediaWikiAWSCiCd/master/InstallMediaWikiEC2AWS.sh | bash
+
 sudo yum install -y httpd php php-mysql php-gd php-xml mysql-server mysql
 sudo yum -y update
 sudo service mysqld start
@@ -8,4 +9,7 @@ echo -e "now perform the below steps "
 echo -e "DB login using \n mysql -u root -p /n create wiki user using \n CREATE USER 'wiki'@'localhost' IDENTIFIED BY 'Password'; \n create DATABASE \n CREATE DATABASE wikidatabase; \n Grant privileges to newly created DB using command \n GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost'; \n FLUSH PRIVILEGES; \n now display database created using \n SHOW DATABASES;\n To display what has been granted use command \n SHOW GRANTS FOR 'wiki'@'localhost'; \n now exit \n exit"
 
 echo -e "once above steps have been completed run second script "
+
+#wget -O - hhttps://raw.githubusercontent.com/ImmLearning/MediaWikiAWSCiCd/master/secondscript.sh | bash
+
 
